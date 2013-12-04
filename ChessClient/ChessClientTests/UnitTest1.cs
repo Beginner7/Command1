44af299f4814f6d573b40b57cb244cda1b888a49
EgorRudakov <egor-rudakov@mail.ru>
2013-12-04 16:03:15 +0300

    Удалил тесты на подключение

--- ChessClient/ChessClientTests/UnitTest1.cs
+++ ChessClient/ChessClientTests/UnitTest1.cs
@@ -5,27 +5,27 @@ using ChessClient;
 
 namespace ChessClientTests
 {
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void ServerConnection()
-        {
-            Server server = new Server("http://command1.apphb.com/");
-            //45878
-            User usr = server.Login("lol", "lol");
-            Assert.AreEqual((UInt64)45878, usr.Id);
+    //[TestClass]
+    //public class UnitTest1
+    //{
+    //    [TestMethod]
+    //    public void ServerConnection()
+    //    {
+    //        Server server = new Server("http://command1.apphb.com/");
+    //        //45878
+    //        User usr = server.Login("lol", "lol");
+    //        Assert.AreEqual((UInt64)45878, usr.Id);
 
-        }
+    //    }
 
-        [TestMethod]
-        public void Registration()
-        {
-            Server server = new Server("http://command1.apphb.com/");
-            User usr = server.Register("login", "password");
-            Assert.AreEqual((UInt64)000, usr.Id);
-            Assert.AreEqual("login", usr.Login);
-            Assert.AreEqual("password", usr.Password);
-        }
-    }
+    //    [TestMethod]
+    //    public void Registration()
+    //    {
+    //        Server server = new Server("http://command1.apphb.com/");
+    //        User usr = server.Register("login", "password");
+    //        Assert.AreEqual((UInt64)000, usr.Id);
+    //        Assert.AreEqual("login", usr.Login);
+    //        Assert.AreEqual("password", usr.Password);
+    //    }
+    //}
 }
