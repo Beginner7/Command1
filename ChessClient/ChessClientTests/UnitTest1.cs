82b4701a4e056723f51b01e1ee86b50ef7befba5
EgorRudakov <egor-rudakov@mail.ru>
2013-12-18 16:50:14 +0300

    DbConnectionTest

--- ChessClient/ChessClientTests/UnitTest1.cs
+++ ChessClient/ChessClientTests/UnitTest1.cs
@@ -4,6 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ChessClient;
 using System.Net.Http;
 using System.Net;
+using System.Data.SqlClient;
 
 namespace ChessClientTests
 {
@@ -33,5 +34,17 @@ namespace ChessClientTests
             Assert.AreEqual("login", usr.Login);
             Assert.AreEqual("password", usr.Password);
         }
+        [TestMethod]
+        public void DBConnect()
+        {
+            var connection = new SqlConnection("Server=20a06137-e97e-4681-a96b-a28300d56ae5.sqlserver.sequelizer.com;Database=db20a06137e97e4681a96ba28300d56ae5;User ID=wpkumwollezvaiac;Password=Su6gQSHSsNJt4YGfF7ou7h76jteYWYYm8bmP2tvicPDYroEKJXscgNiKwa5wMR5B;");
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM Client";
+            command.CommandTimeout = 20;
+            command.CommandType = System.Data.CommandType.Text;
+            command.ExecuteReader();
+        }
+
     }
 }
